43f10abb4362e1038bc42b8042c2118e5509b353
Robert Lewis <ralewis85@gmail.com>
2025-11-22 12:23:48 -0800

    Initial Commit

---
+++ aftermath/aftermathApp.swift
@@ -0,0 +1,17 @@
+//
+//  aftermathApp.swift
+//  aftermath
+//
+//  Created by Robert Lewis on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct aftermathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
